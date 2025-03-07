2177cb25d1059a6db21a08c9fdbcefd5d826f3e4
TeodoraAndreeaDumitrascu <teodoraandreea.dumitrascu@ulbsibiu.ro>
2025-03-07 10:58:12 +0200

    Lab 2

---
+++ src/ro/ulbs/paradigme/lab2/Form.java
@@ -0,0 +1,27 @@
+package ro.ulbs.paradigme.lab2;
+
+public class Form {
+    private String color;
+
+    public Form() {
+    }
+    public Form(String color){
+        this.color=color;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+    public float getArea(){
+        return 0;
+    }
+    @Override
+    public String toString(){
+        return "This form has the color" + color;
+    }
+}
+
+
---
+++ src/ro/ulbs/paradigme/lab2/Main.java
@@ -0,0 +1,10 @@
+package ro.ulbs.paradigme.lab2;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+Form a= new Form("white");
+        System.out.println("Culoarea este" + " " +  a.getColor());
+        }
+    }
